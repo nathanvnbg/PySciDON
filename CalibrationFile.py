6e227c349bd6d918890638993a92294fbe5989bf
Nathan VandenBerg <nathanvn@uvic.ca>
2016-07-20 15:08:50 -0700

    Added verification method when reading raw files

--- CalibrationFile.py
+++ CalibrationFile.py
@@ -15,7 +15,7 @@ class CalibrationFile:
         self.m_id = ""
         self.m_name = ""
         self.m_data = []
-        
+
         self.m_instrumentType = ""
         self.m_media = ""
         self.m_measMode = ""
@@ -26,8 +26,8 @@ class CalibrationFile:
     def printd(self):
         if len(self.m_id) != 0:
             print("id:", self.m_id)
-        for cd in self.m_data:
-            cd.printd()
+#        for cd in self.m_data:
+#            cd.printd()
 
     # Reads a calibration file and generates calibration data
     def read(self, f):
@@ -72,8 +72,60 @@ class CalibrationFile:
                 return cd.m_units
         return None
 
+    # Returns the sensor type
+    def getSensorType(self):
+        for cd in self.m_data:
+            if cd.m_type == "ES" or \
+               cd.m_type == "LI" or \
+               cd.m_type == "LT":
+                return cd.m_type
+        return "None"
+
+    # Verify raw data message can be read successfully
+    def verifyRaw(self, msg):
+        try:
+            nRead = 0
+            for i in range(0, len(self.m_data)):
+                v = 0
+                cd = self.m_data[i]
+
+                # Read variable length message frames (field length == -1)
+                if cd.m_fieldLength == -1:
+                    delimiter = self.m_data[i+1].m_units
+                    delimiter = delimiter.encode("utf-8").decode("unicode_escape").encode("utf-8")
+                    #print("delimiter:", delimiter)
+
+                    end = msg[nRead:].find(delimiter)
+                    #print("read:", nRead, end)
+                    b = msg[nRead:nRead+end]
+                    v = cd.convertRaw(b)
+
+                    nRead += end
+
+                # Read fixed length message frames
+                else:
+                    if cd.m_fitType.upper() != "DELIMITER":
+                        if cd.m_fieldLength != 0:
+                            b = msg[nRead:nRead+cd.m_fieldLength]
+                            #print(nRead, cd.m_fieldLength, b)
+                            v = cd.convertRaw(b)
+                    nRead  += cd.m_fieldLength
+                
+                # Passed EndOfFile
+                #if nRead > len(msg):
+                #    return False
+
+            return True
+
+        except:
+            pass
+            #print("Failed to read message successfully")
+
+        return False
+
 
     # Reads a message frame from the raw file and generates hdf groups/datasets
+    # Returns nRead (number of bytes read) or -1 on error
     def convertRaw(self, msg, gp):
         nRead = 0
 
@@ -81,11 +133,17 @@ class CalibrationFile:
         #    self.m_data[i].printd()
         #print("file:", msg)
 
+        if self.verifyRaw(msg) == False:
+            return -1
+
         for i in range(0, len(self.m_data)):
             v = 0
             cd = self.m_data[i]
 
-            # Read variable length message frames, field length == -1
+
+            # Get value from message frame
+
+            # Read variable length message frames (field length == -1)
             if cd.m_fieldLength == -1:
                 delimiter = self.m_data[i+1].m_units
                 delimiter = delimiter.encode("utf-8").decode("unicode_escape").encode("utf-8")
@@ -107,12 +165,17 @@ class CalibrationFile:
                         v = cd.convertRaw(b)
                 nRead  += cd.m_fieldLength
 
+
+            # Stores value in dataset or attribute depending on type
+
             # Stores raw data into hdf datasets according to type
             if cd.m_fitType.upper() != "NONE" and cd.m_fitType.upper() != "DELIMITER":
                 cdtype = cd.m_type.upper()
                 if cdtype != "INSTRUMENT" and cdtype != "VLF_INSTRUMENT" and \
                    cdtype != "SN" and cdtype != "VLF_SN":
                     ds = gp.getDataset(cd.m_type)
+                    if ds is None:
+                        ds = gp.addDataset(cd.m_type)
                     #print(cd.m_id)
                     #ds.m_temp.append(v)
                     #ds.addColumn(cd.m_id)
@@ -149,6 +212,8 @@ class CalibrationFile:
             nRead += 3
             #print("Date:",v)
             ds1 = gp.getDataset("DATETAG")
+            if ds1 is None:
+                ds1 = gp.addDataset("DATETAG")
             ds1.appendColumn(u"NONE", v)
 
             b = msg[nRead:nRead+4]
@@ -159,6 +224,8 @@ class CalibrationFile:
             nRead += 4
             #print("Time:",v)
             ds1 = gp.getDataset("TIMETAG2")
+            if ds1 is None:
+                ds1 = gp.addDataset("TIMETAG2")
             ds1.appendColumn(u"NONE", v)
 
         return nRead
