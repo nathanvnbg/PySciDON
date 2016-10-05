4a6f6fafa7dfee724561d5062cbb0890fd3e53c6
Nathan VandenBerg <nathanvn@uvic.ca>
2016-10-05 16:11:18 -0700

    Updated Controller to read/write files before and after processing, and updated saving to csv

--- Controller.py
+++ Controller.py
@@ -8,6 +8,7 @@ import h5py
 import numpy as np
 #import scipy as sp
 
+from CSVWriter import CSVWriter
 from CalibrationFileReader import CalibrationFileReader
 #from RawFileReader import RawFileReader
 from HDFRoot import HDFRoot
@@ -83,128 +84,177 @@ class Controller:
         (dirpath, filename) = os.path.split(fp)
         name = os.path.splitext(filename)[0]
         
-        print("ProcessL1a")
-
-        nm = os.path.join(dirpath, name + "_L0.txt")
-        f = open(nm, 'w')
+        # Create "L0" file to save program start time
+        #nm = os.path.join(dirpath, name + "_L0.txt")
+        #f = open(nm, 'w')
         
-        fp = os.path.join(dirpath, filename)
-
-        root = ProcessL1a.processL1a(calibrationMap, fp)
-        root.writeHDF5(os.path.join(dirpath, name + "_L1a.hdf"))
-
+        filepath = os.path.join(dirpath, filename)
+        if not os.path.isfile(filepath):
+            return None
+        print("ProcessL1a")
+        root = ProcessL1a.processL1a(calibrationMap, filepath)
+        if root is not None:
+            root.writeHDF5(os.path.join(dirpath, name + "_L1a.hdf"))
         return root
 
     def processL1b(root, fp, calibrationMap):
         (dirpath, filename) = os.path.split(fp)
         filename = os.path.splitext(filename)[0]
+        filepath = os.path.join(dirpath, filename + "_L1a.hdf")
+        if not os.path.isfile(filepath):
+            return None
         print("ProcessL1b")
-        root = HDFRoot.readHDF5(os.path.join(dirpath, filename + "_L1a.hdf"))
+        root = HDFRoot.readHDF5(filepath)
         #print("HDFFile:")
         #root.printd()
         root = ProcessL1b.processL1b(root, calibrationMap)
-        root.writeHDF5(os.path.join(dirpath, filename + "_L1b.hdf"))
+        if root is not None:
+            root.writeHDF5(os.path.join(dirpath, filename + "_L1b.hdf"))
         return root
 
     @staticmethod
     def processL2(root, fp):
         (dirpath, filename) = os.path.split(fp)
         filename = os.path.splitext(filename)[0]
+        filepath = os.path.join(dirpath, filename + "_L1b.hdf")
+        if not os.path.isfile(filepath):
+            return None
         print("ProcessL2")
-        root = HDFRoot.readHDF5(os.path.join(dirpath, filename + "_L1b.hdf"))
+        root = HDFRoot.readHDF5(filepath)
         root = ProcessL2.processL2(root)
-        root.writeHDF5(os.path.join(dirpath, filename + "_L2.hdf"))
+        if root is not None:
+            root.writeHDF5(os.path.join(dirpath, filename + "_L2.hdf"))
         return root
 
     @staticmethod
     def processL2s(root, fp):
         (dirpath, filename) = os.path.split(fp)
         filename = os.path.splitext(filename)[0]
+        filepath = os.path.join(dirpath, filename + "_L2.hdf")
+        if not os.path.isfile(filepath):
+            return None
         print("ProcessL2s")
-        root = HDFRoot.readHDF5(os.path.join(dirpath, filename + "_L2.hdf"))
+        root = HDFRoot.readHDF5(filepath)
         root = ProcessL2s.processL2s(root)
         #root.printd()
-        root.writeHDF5(os.path.join(dirpath, filename + "_L2s.hdf"))
+        if root is not None:
+            root.writeHDF5(os.path.join(dirpath, filename + "_L2s.hdf"))
         return root
 
     @staticmethod
     def processL3a(root, fp):
         (dirpath, filename) = os.path.split(fp)
         filename = os.path.splitext(filename)[0]
+        filepath = os.path.join(dirpath, filename + "_L2s.hdf")
+        if not os.path.isfile(filepath):
+            return None
         print("ProcessL3a")
-        root = HDFRoot.readHDF5(os.path.join(dirpath, filename + "_L2s.hdf"))
+        root = HDFRoot.readHDF5(filepath)
         root = ProcessL3a.processL3a(root)
-        root.writeHDF5(os.path.join(dirpath, filename + "_L3a.hdf"))
+        if root is not None:
+            root.writeHDF5(os.path.join(dirpath, filename + "_L3a.hdf"))
         return root
 
     @staticmethod
     def processL4(root, fp):
         (dirpath, filename) = os.path.split(fp)
         filename = os.path.splitext(filename)[0]
+        filepath = os.path.join(dirpath, filename + "_L3a.hdf")
+        if not os.path.isfile(filepath):
+            return None
         print("ProcessL4")
-        root = HDFRoot.readHDF5(os.path.join(dirpath, filename + "_L3a.hdf"))
+        root = HDFRoot.readHDF5(filepath)
         root = ProcessL4.processL4(root)
         if root is not None:
             Utilities.plotReflectance(root, filename)
             root.writeHDF5(os.path.join(dirpath, filename + "_L4.hdf"))
-
-            # Test saving to txt
-            csvPath = os.path.join(dirpath, filename + ".csv")
-            with open(csvPath, 'w') as f:
-                gp = root.getGroup("Reflectance")
-                esDS = gp.getDataset("ES")
-                liDS = gp.getDataset("LI")
-                ltDS = gp.getDataset("LT")
-                rrsDS = gp.getDataset("Rrs")
-                #np.savetxt('Data/test.out', ds.m_data)
-                outList = []
-                ls = ["wl"] + [k for k,v in sorted(rrsDS.m_data.dtype.fields.items(), key=lambda k: k[1])]
-                outList.append(ls)
-                #line = 'wl,' + ','.join([k for k,v in sorted(rrsDS.m_data.dtype.fields.items(), key=lambda k: k[1])])
-                #f.write(line)
-                #f.write('\n')
-                #line = "es," + ','.join(['%f' % num for num in esDS.m_data[0]])
-                #f.write(line)
-                #f.write('\n')
-                #line = "li," + ','.join(['%f' % num for num in liDS.m_data[0]])
-                #f.write(line)
-                #f.write('\n')
-                #line = "lt," + ','.join(['%f' % num for num in ltDS.m_data[0]])
-                #f.write(line)
-                #f.write('\n')
-                #line = "rrs," + ','.join(['%f' % num for num in rrsDS.m_data[0]])
-                #f.write(line)
-                #f.write('\n')
-
-                ls = ["es"] + ['%f' % num for num in esDS.m_data[0]]
-                outList.append(ls)
-                ls = ["li"] + ['%f' % num for num in liDS.m_data[0]]
-                outList.append(ls)
-                ls = ["lt"] + ['%f' % num for num in ltDS.m_data[0]]
-                outList.append(ls)
-                ls = ["rrs"] + ['%f' % num for num in rrsDS.m_data[0]]
-                outList.append(ls)
-                outList = zip(*outList)
-                writer = csv.writer(f)
-                writer.writerows(outList)
-            
         return root
+
+
+
+    # Saving data to formatted csv file
+    @staticmethod
+    def outputCSV_L4(fp):
+        (dirpath, filename) = os.path.split(fp)
+        filename = os.path.splitext(filename)[0]
+
+        filepath = os.path.join(dirpath, filename + "_L4.hdf")
+        if not os.path.isfile(filepath):
+            return
+
+        root = HDFRoot.readHDF5(filepath)
+        if root is None:
+            print("outputCSV: root is None")
+            return
+
+        Controller.outputCSV(fp, root, "Reflectance", "ES")
+        Controller.outputCSV(fp, root, "Reflectance", "LI")
+        Controller.outputCSV(fp, root, "Reflectance", "LT")
+        Controller.outputCSV(fp, root, "Reflectance", "Rrs")
+
+
+    @staticmethod
+    def outputCSV(fp, root, gpName, dsName):
+        (dirpath, filename) = os.path.split(fp)
+        filename = os.path.splitext(filename)[0]
+
+        gp = root.getGroup(gpName)
+        ds = gp.getDataset(dsName)
+        #np.savetxt('Data/test.out', ds.m_data)
+
+        if not ds:
+            print("Warning - outputCSV: missing dataset")
+            return
+
+        dirpath = "csv"
+        name = filename[28:43]
+
+        outList = []
+        columnName = dsName.lower()
+
+        total = ds.m_data.shape[0]
+        #ls = ["wl"] + [k for k,v in sorted(ds.m_data.dtype.fields.items(), key=lambda k: k[1])]
+        ls = ["wl"] + list(ds.m_data.dtype.names)
+        outList.append(ls)
+        for i in range(total):
+            n = str(i+1)
+            ls = [columnName + "_" + name + '_' + n] + ['%f' % num for num in ds.m_data[i]]
+            outList.append(ls)
+
+        outList = zip(*outList)
+
+        filename = dsName.upper() + "_" + name
+        #filename = name + "_" + dsName.upper()
+        csvPath = os.path.join(dirpath, filename + ".csv")
+
+        with open(csvPath, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(outList)
+
 
     @staticmethod
     def processAll(fp, calibrationMap):
+        print("Processing: " + fp)
         root = HDFRoot()
-        root = Controller.processL1a(root, fp, calibrationMap)
-        root = Controller.processL1b(root, fp, calibrationMap)
-        root = Controller.processL2(root, fp)
-        root = Controller.processL2s(root, fp)
-        root = Controller.processL3a(root, fp)
+        #root = Controller.processL1a(root, fp, calibrationMap)
+        #root = Controller.processL1b(root, fp, calibrationMap)
+        #root = Controller.processL2(root, fp)
+        #root = Controller.processL2s(root, fp)
+        #root = Controller.processL3a(root, fp)
         root = Controller.processL4(root, fp)
+        #Controller.outputCSV_L4(fp)
+        #CSVWriter.outputTXT_L1a(fp)   
+        #CSVWriter.outputTXT_L1b(fp)
+        #CSVWriter.outputTXT_L2(fp)
+        #CSVWriter.outputTXT_L2s(fp)
+        #CSVWriter.outputTXT_L3a(fp)
+        #CSVWriter.outputTXT_L4(fp)
         #root = HDFRoot.readHDF5(os.path.join(dirpath, filename + "_L3a.hdf"))
 
     @staticmethod
     def processDirectory(path, calibrationMap):
         for (dirpath, dirnames, filenames) in os.walk(path):
-            for name in filenames:
+            for name in sorted(filenames):
                 #print("infile:", name)
                 if os.path.splitext(name)[1].lower() == ".raw":
                     Controller.processAll(os.path.join(dirpath, name), calibrationMap)
